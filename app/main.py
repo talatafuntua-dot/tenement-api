aa9382791b4cabd23021bd5ecbb394f7854a3ea9
talatafuntua-dot <talatafuntua@gmail.com>
2026-08-14 10:56:02 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,30 +2,25 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.properties import router as properties_router
 from app.routers import pdf
-from app.routers.reports import router as reports_router
-
 
 app = FastAPI(
-    title="Tenement Rate Management API",
-    version="1.0.0"
+title="Tenement Rate Management API",
+version="1.0.0"
 )
 
-# Allow requests from your WordPress site
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
 )
 
 app.include_router(properties_router)
 app.include_router(pdf.router)
-app.include_router(reports_router)
-
 
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to the Tenement Rate Management API"
-    }
+return {
+"message": "Welcome to the Tenement Rate Management API"
+}
