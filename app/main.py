fa4837e508c19625c75df8b00baac5944158bff9
talatafuntua-dot <talatafuntua@gmail.com>
2026-08-14 10:03:22 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.properties import router as properties_router
 from app.routers import pdf
-
+from app.routers.reports import router as reports_router
 
 
 app = FastAPI(
@@ -13,7 +13,7 @@ app = FastAPI(
 # Allow requests from your WordPress site
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # Change this later to your WordPress domain
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,6 +21,8 @@ app.add_middleware(
 
 app.include_router(properties_router)
 app.include_router(pdf.router)
+app.include_router(reports_router)
+
 
 @app.get("/")
 def home():
