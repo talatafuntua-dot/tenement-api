dfab23361c2e39223180ea03ab017ea65f74b8d7
talatafuntua-dot <talatafuntua@gmail.com>
2026-08-18 05:18:13 -0700

    Update data.py

--- app/routers/data.py
+++ app/routers/data.py
@@ -1,6 +1,9 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from fastapi import APIRouter
+from app.database import get_db
 from app.models import Property
+
 
 router = APIRouter(
     prefix="/data",
@@ -10,9 +13,6 @@ router = APIRouter(
 
 @router.get("/fields")
 def get_data_fields():
-    """
-    Return all available Property fields.
-    """
 
     fields = [
         column.name
@@ -21,4 +21,33 @@ def get_data_fields():
 
     return {
         "fields": fields
+    }
+
+
+@router.get("/records")
+def get_data_records(
+    db: Session = Depends(get_db)
+):
+
+    properties = db.query(Property).all()
+
+    records = []
+
+    for property in properties:
+
+        record = {}
+
+        for column in Property.__table__.columns:
+
+            value = getattr(property, column.name)
+
+            if value is not None:
+                record[column.name] = value
+            else:
+                record[column.name] = None
+
+        records.append(record)
+
+    return {
+        "records": records
     }
